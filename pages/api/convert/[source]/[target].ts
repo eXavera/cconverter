19f4c17da3f2e3ec0d8db4ce47352c1060990ea3
Stanislav Fajfr <sfajfr@purple-technology.com>
2022-11-26 16:46:22 +0100

    Basic functional rate conversion

--- pages/api/convert/[source]/[target].ts
+++ pages/api/convert/[source]/[target].ts
@@ -1,10 +1,6 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { ConversionResponse } from '../../../../api-interface/convert'
 import type { NextApiRequest, NextApiResponse } from 'next'
-
-type Data = {
-  result: number,
-  data: any
-}
+import { setTimeout } from 'timers/promises'
 
 type Currency = string
 
@@ -12,29 +8,54 @@ interface IApiResponse {
   rates: Record<Currency, number>
 }
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  const sourceCurrency = <Currency>req.query['source']
-  const targetCurrency = <Currency>req.query['target']
-  const amount : number = parseInt(<string>req.query['amount'], 10)
-
+const getUsdRate = async function(targetCurrency: Currency) : Promise<number> {
   const apiParams = new URLSearchParams([
-    ['app_id', '<blbe>'],
-    ['base', sourceCurrency],
+    ['app_id', '19e5783e6cd041899952b9a962164469'],
+    ['base', 'USD'],
     ['symbols', targetCurrency]
   ])
 
   const apiHttpResp = await fetch('https://openexchangerates.org/api/latest.json?' + apiParams.toString())
   const apiResponse = <IApiResponse>(await apiHttpResp.json())
 
-  const rate = apiResponse.rates[targetCurrency]
+  return apiResponse.rates[targetCurrency]
+}
+
+const convert = async function(sourceCurrency: Currency, targetCurrency: Currency, amount: number) : Promise<number> {
+  if (sourceCurrency === 'USD') {
+    return amount * await getUsdRate(targetCurrency)
+  }
+  else if(targetCurrency === 'USD') {
+    return amount / await getUsdRate(sourceCurrency)
+  }
+  return -1; // report error
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ConversionResponse>
+) {
+  const sourceCurrency = <Currency>req.query['source']
+  const targetCurrency = <Currency>req.query['target']
+  const amount: number = parseInt(<string>req.query['amount'], 10)
+
+  if (sourceCurrency === targetCurrency) {
+    await setTimeout(1500)
+
+    res.status(200).json({
+      result: amount
+    })
+    return
+  }
+
+  if (amount === 0) {
+    res.status(200).json({
+      result: amount
+    })
+    return
+  }
 
   res.status(200).json({
-    result: Math.round(amount * rate),
-    data: {
-      query: 'https://openexchangerates.org/api/latest.json?' + apiParams.toString(),
-    }
+    result: Math.round(await convert(sourceCurrency, targetCurrency, amount))
   })
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,85 +1,140 @@
-import React, { useId } from 'react'
-import { GetStaticProps } from 'next'
-import { Form, Formik, FormikHelpers } from 'formik'
+import React, { useState } from 'react'
+import { GetServerSideProps, GetStaticProps } from 'next'
+import { Form, Formik, FormikErrors, FormikHelpers } from 'formik'
 import {
     Flex,
     NumberInput,
     SelectOption,
     SelectNative,
-    Button
+    Button,
+    Box,
+    Text,
+    Spinner,
+    Notice
 } from '@purple/phoenix-components'
 import { ILayoutProps } from '../components/layout'
+import { ConversionResponse } from '../api-interface/convert'
+
+interface IConverterPageProps {
+    supportedCurrencies: string[]
+}
 
 interface IConvertFormProps {
     sourceAmount: number,
     sourceCurrency: SelectOption,
-    targetCurrency: SelectOption
+    targetCurrency: SelectOption,
+    validationError?: string
 }
 
-const currencies: SelectOption[] = [{
-    label: 'USD',
-    value: 'USD'
-}, {
-    label: 'EUR',
-    value: 'EUR'
-}]
-
-const initialValues: IConvertFormProps = {
-    sourceAmount: 0,
-    sourceCurrency: currencies[0],
-    targetCurrency: currencies[0]
-}
-
-const doSubmit = async function (values: IConvertFormProps, formControls: FormikHelpers<IConvertFormProps>): Promise<any>{
+const doSubmit = async function (values: IConvertFormProps, formControls: FormikHelpers<IConvertFormProps>): Promise<number> {
     console.log(values)
 
     const resp = await fetch(`/api/convert/${values.sourceCurrency.value}/${values.targetCurrency.value}?amount=${(values.sourceAmount * 100).toFixed(0)}`)
-    console.log(resp)
-    const result = await resp.json()
-    console.log(result)
+    const result = (await resp.json()) as ConversionResponse
+
+    return result.result / 100
 }
 
-export default function Inndex(): React.ReactNode {
+const ConversionResult: React.FC<{ amount: number, currency: string }> = (props) => {
     return (
-        <Formik<IConvertFormProps> initialValues={initialValues} onSubmit={doSubmit}>
+        <Box>
+            <Text size="large"> = {props.amount.toFixed(2)} {props.currency}</Text>
+        </Box>
+    )
+}
+
+const IndexPage: React.FC<IConverterPageProps> = ({ supportedCurrencies }) => {
+    const [targetAmount, setTargetAmount] = useState(0)
+    const [targeCurrency, setTargetCurrency] = useState('USD')
+    const [isLoading, setIsLoading] = useState(false)
+
+    const allCurrencyItems = supportedCurrencies.map<SelectOption>(name => ({
+        label: name,
+        value: name
+    }))
+
+    const usd = allCurrencyItems.find(curr => curr.value === 'USD') as SelectOption
+
+    const initialValues: IConvertFormProps = {
+        sourceAmount: 0,
+        sourceCurrency: usd,
+        targetCurrency: usd
+    }
+
+    const validateForm = (values: IConvertFormProps): FormikErrors<IConvertFormProps> => {
+        if (values.sourceCurrency.value !== usd.value && values.targetCurrency.value != usd.value) {
+            return {
+                validationError: 'One of the currency has to be USD'
+            }
+        }
+
+        return {}
+    }
+
+    return (
+        <Formik<IConvertFormProps> initialValues={initialValues} validate={validateForm} onSubmit={async (values, controls) => {
+            setIsLoading(true)
+            const resultAmount = await doSubmit(values, controls)
+            setTargetAmount(resultAmount)
+            setTargetCurrency(values.targetCurrency.label)
+            setIsLoading(false)
+        }}>
             {(props): React.ReactNode => {
-                const { values, setFieldValue, handleSubmit } = props;
+                const { values, setFieldValue, handleSubmit, errors } = props;
+
+                console.log('errors', errors)
 
                 return (
-                    <Form onSubmit={handleSubmit}>
-                        <Flex mt="2em" mb="1em">
-                            <NumberInput
-                                label='Source Amount'
-                                name='sourceAmount'
-                                value={values.sourceAmount}
-                                onChange={(amount => setFieldValue('sourceAmount', amount))} />
-                            <SelectNative
-                                label='Source Currency'
-                                name='sourceCurrency'
-                                value={values.sourceCurrency}
-                                options={currencies}
-                                onChange={(selectedCurrency) => setFieldValue('sourceCurrency', selectedCurrency)} />
-                            <SelectNative
-                                label='Target Currency'
-                                name='targetCurrency'
-                                value={values.targetCurrency}
-                                options={currencies}
-                                onChange={(selectedCurrency) => setFieldValue('targetCurrency', selectedCurrency)} />
-                        </Flex>
-                        <Button icon="play-circle" iconAlignment='right' type="submit">
-                            Convert
-                        </Button>
-                    </Form>
+                    <Flex alignItems="stretch" flexDirection="column">
+                        <Form onSubmit={handleSubmit}>
+                            <Flex mt="2em" mb="1em">
+                                <NumberInput
+                                    label='Source Amount'
+                                    name='sourceAmount'
+                                    value={values.sourceAmount}
+                                    onChange={(amount => setFieldValue('sourceAmount', amount))} />
+                                <SelectNative
+                                    label='Source Currency'
+                                    name='sourceCurrency'
+                                    value={values.sourceCurrency}
+                                    options={allCurrencyItems}
+                                    onChange={(selectedCurrency) => setFieldValue('sourceCurrency', selectedCurrency)} />
+                                <SelectNative
+                                    label='Target Currency'
+                                    name='targetCurrency'
+                                    disabled={isLoading}
+                                    value={values.targetCurrency}
+                                    options={allCurrencyItems}
+                                    onChange={(selectedCurrency) => setFieldValue('targetCurrency', selectedCurrency)} />
+                                <Button
+                                    icon="play-circle"
+                                    iconAlignment='right'
+                                    type="submit"
+                                    loading={isLoading}
+                                    disabled={isLoading}
+                                    ml="1em">
+                                    Convert
+                                </Button>
+                            </Flex>
+                        </Form>
+                        {isLoading ? <Spinner size="large" /> : <ConversionResult amount={targetAmount} currency={targeCurrency} />}
+                        {errors.validationError && <Notice colorTheme="warning" mt="1em">{errors.validationError}</Notice>}
+                    </Flex>
                 )
             }}
         </Formik>
     )
 }
 
-export const getStaticProps: GetStaticProps<ILayoutProps> = () => {
+export default IndexPage
+
+export const getServerSideProps: GetServerSideProps<ILayoutProps> = async () => {
+    const res = await fetch(`https://openexchangerates.org/api/currencies.json`)
+    const data = await res.json()
     return {
         props: {
-            title: 'Converter'
+            title: 'Converter',
+            supportedCurrencies: Object.keys(data)
         }
     }
 }
